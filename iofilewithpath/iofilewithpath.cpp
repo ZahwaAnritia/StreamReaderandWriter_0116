f4bbfe8308b9f46af67bbd8bad76219795795005
ZahwaAnritia <anritia15@gmail.com>
2024-06-03 10:21:48 +0700

    menampilkan kondisi jika file ada dan tidak ada

--- iofilewithpath/iofilewithpath.cpp
+++ iofilewithpath/iofilewithpath.cpp
@@ -21,4 +21,23 @@ int main() {
 		if (baris == "q") break;
 		outfile << baris << endl;
 	}
+
+	outfile.close();
+
+	ifstream infile;
+
+	infile.open(NamaFile + ".txt, ios::in");
+	cout << endl << ".= Membuka dan membaca file" << endl;
+
+	if (infile.is_open())
+	{
+		while (getline(infile, baris))
+		{
+			cout << baris << '\n';
+		}
+
+		infile.close();
+	}
+	else cout << "Unable to open file";
+	return 0;
 }
